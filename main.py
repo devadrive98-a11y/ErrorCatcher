3bcf2c9726c49ede61f41032ee1c709437c6ae6c
devadrive98-a11y <devadrive98@gmail.com>
2026-07-10 16:13:59 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,22 @@
-from fastapi import FastAPI, Request, HTTPException
+from fastapi import FastAPI, Request
 import requests
 import os
 
 app = FastAPI()
 
-# ambil dari environment variable
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
-
-# validasi saat startup
-@app.on_event("startup")
-def startup():
-    print("🚀 APP STARTED")
-
-    if not BOT_TOKEN:
-        print("❌ BOT_TOKEN tidak ditemukan di ENV")
-    if not CHAT_ID:
-        print("❌ CHAT_ID tidak ditemukan di ENV")
-
+# isi dengan punyamu
+BOT_TOKEN = "8850208766:AAGbViUjrEMZ0dZpXgOu30c-dAJdgm3X4E8"
+CHAT_ID = "1007355104"
 
 def send_telegram(message: str):
-    if not BOT_TOKEN or not CHAT_ID:
-        print("❌ ENV belum diset, skip kirim telegram")
-        return
-
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-
+    
     payload = {
         "chat_id": CHAT_ID,
-        "text": message,
-        "parse_mode": "HTML"
+        "text": message
     }
 
-    try:
-        res = requests.post(url, json=payload, timeout=10)
-        print("Telegram response:", res.status_code, res.text)
-    except Exception as e:
-        print("❌ Gagal kirim ke Telegram:", str(e))
+    requests.post(url, json=payload)
 
 
 @app.get("/")
@@ -46,18 +26,15 @@ def root():
 
 @app.post("/error")
 async def receive_error(request: Request):
-    try:
-        data = await request.json()
-    except:
-        raise HTTPException(status_code=400, detail="Invalid JSON")
+    data = await request.json()
 
     message = f"""
-🚨 <b>ERROR APP</b>
+🚨 ERROR APP
 
-<b>Message:</b> {data.get("message")}
-<b>StackTrace:</b> {data.get("stackTrace")}
-<b>Device:</b> {data.get("device")}
-<b>Time:</b> {data.get("time")}
+Message: {data.get("message")}
+StackTrace: {data.get("stackTrace")}
+Device: {data.get("device")}
+Time: {data.get("time")}
 """
 
     send_telegram(message)
