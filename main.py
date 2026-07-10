56945aef74b06ff4f371810ee8259de750d65cee
devadrive98-a11y <devadrive98@gmail.com>
2026-07-10 15:38:21 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI, Request
+import requests
+import os
+
+app = FastAPI()
+
+# isi dengan punyamu
+BOT_TOKEN = "8675165377:AAHvkkmkixGluJOQGO2APtgkimp6XhMKcsA"
+CHAT_ID = "1007355104"
+
+def send_telegram(message: str):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    
+    payload = {
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+
+    requests.post(url, json=payload)
+
+
+@app.get("/")
+def root():
+    return {"status": "ok"}
+
+
+@app.post("/error")
+async def receive_error(request: Request):
+    data = await request.json()
+
+    message = f"""
+🚨 ERROR APP
+
+Message: {data.get("message")}
+StackTrace: {data.get("stackTrace")}
+Device: {data.get("device")}
+Time: {data.get("time")}
+"""
+
+    send_telegram(message)
+
+    return {"status": "sent"}
